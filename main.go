44d265ac060bac3494065226fa6ddf15f72a622f
poppedbit <poppedbit@gmail.com>
2024-04-26 17:30:25 -0600

    Some learning

--- main.go
+++ main.go
@@ -9,15 +9,51 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type Pet struct {
+	name string
+	age  int
+}
+
+type Cat struct {
+	hasClaws bool
+}
+
 func main() {
 
+	// ENV
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
 
-	port := os.Getenv("PORT")
+	// Arrays
+	myList := []int{1, 2, 3, 4, 5}
 
+	// For Loop
+	for i := 0; i < len(myList); i++ {
+		// If, else if, else
+		if myList[i] < 3 {
+
+		} else if myList[i] < 5 {
+			continue
+		} else {
+			break
+		}
+	}
+
+	// While Loops
+	n := 1
+	for n < 5 {
+		n += 1
+	}
+
+	// For Each
+	for i, s := range []string{"Hello", "World", "!"} {
+		fmt.Println(i, s)
+	}
+
+	// HTTP
+	port := os.Getenv("PORT")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
